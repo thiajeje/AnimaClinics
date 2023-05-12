5779b5ec4daf9057c28a751f7270c45e03a043ce
Thiago Ajeje <63625421+thiajeje@users.noreply.github.com>
2023-05-11 21:21:49 -0300

    Update UsuarioRepository.java

--- src/main/java/com/example/demo5/repositories/UsuarioRepository.java
+++ src/main/java/com/example/demo5/repositories/UsuarioRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo5.repositories;
 
-import com.example.demo5.domain.Cliente;
+import com.example.demo5.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepositoryJPA extends JpaRepository<Cliente, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
